b1e8eb3d404420645f020d899e9a2f25a4641f9b
Muhammad shiraz <muhammadshiraz492@gmail.com>
2022-10-25 22:46:28 +0500

    okey type

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
-var a = "Hello";
+// class App{
+//     test(){
+//         console.log("This is typescript");
+//     }
+// }
+// let a1=new App()
+// a1.test()
+{ /* <Types></Types> */ }
+var a = 12;
 console.log(a);
--- App.tsx
+++ App.tsx
@@ -1,2 +1,14 @@
-let a="Hello"
+// class App{
+//     test(){
+//         console.log("This is typescript");
+        
+//     }
+// }
+
+// let a1=new App()
+// a1.test()
+
+{/* <Types></Types> */}
+
+var a:Number=12;
 console.log(a);
